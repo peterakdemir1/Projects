3e833bac27d3a22016f07e7085b74d5876b3439e
peterakdemir1 <94308786+peterakdemir1@users.noreply.github.com>
2023-09-23 13:02:47 -0400

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,26 @@
-print("hello")
+import streamlit as st
+
+st.markdown("# Where's My Alien?!")
+
+tab1, tab2, tab3 = st.tabs(["Home", "Instructions", "Login"])
+
+with tab1:
+   st.header("Home")
+   st.image("https://media.tenor.com/IIQ-ca9nBu0AAAAd/space.gif")
+   description = '''Welcome to **Where's My Alien?!**
+
+   Find a cheeky little alien in these user-uploaded images and get the best time!
+
+   Upload your own images and send it to friends to see if they can find your alien!
+   
+   May the alien hunt begin!
+   '''
+   st.markdown(description)
+
+with tab2:
+   st.header("Instructions")
+   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+with tab3:
+   st.header("Login")
+   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
